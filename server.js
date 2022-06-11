a2e0b5bbcb1431ee1bdf58091606141294fdd916
PanthaNIS <panthanischal12@gmail.com>
2022-06-11 18:44:52 +0800

    nis

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var cinemaAPI = require("./controllerAPI/api-controller");
 
 var bodyParser=require("body-parser");
 
-// var cors = require('cors')
-// server.use(cors());
+var cors = require('cors')
+server.use(cors());
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:false}));
@@ -18,5 +18,5 @@ server.use(bodyParser.urlencoded({extended:false}));
 server.use("/api/cinema",cinemaAPI);
 
 //start running the web server
-server.listen(3333);
+server.listen(3060);
 console.log("Server is up now and running on port 3060");
